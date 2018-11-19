ee074f7a3fc356164e5ae5fabe7bdcf5f6f59021
Ben Morris ( Insxnity ) <Development@Insxnity.net>
2018-11-19 00:30:19 -0600

    Update v1.0

--- ASG2.js
+++ ASG2.js
@@ -307,6 +307,18 @@ function callCategories() {
         generateBox(key, categoriesEnum, 0, categoriesEnum[key], categoriesInfoText);
     });
 }
+document.onkeydown = function(evt) {
+    evt = evt || window.event;
+    var isEscape = false;
+    if ("key" in evt) {
+        isEscape = (evt.key == "Escape" || evt.key == "Esc");
+    } else {
+        isEscape = (evt.keyCode == 27);
+    }
+    if (isEscape) {
+        callCategories();
+    }
+};
 function callSubset(subsetID) {
     smoothTopScroll();
     hideAllBoxes();
